b272ee434122d922274350e34f332ba718e560bd
Ricardo Tanaka <rtanaka@trainbeyond.com>
2026-08-27 17:46:09 -0500

    Created project

---
+++ Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DVisualStudio262
+{
+    internal class Menu
+    { 
+        public void Execute()
+        {
+            string name;
+            int age;
+
+            Console.WriteLine("Hola!");
+            name = GetName();
+            Console.WriteLine($"Hola {name}");
+            age = GetPositiveNumber("Introduce tu edad:");
+            Console.WriteLine($"Tienes {age} años");
+            Console.WriteLine($"El próximo año tendrás {age+1} años");
+
+        }
+
+        private string GetName()
+        {
+            string name="";
+            while(name.Length==0)
+            {
+                Console.WriteLine("Introduce tu nombre:");
+                name = Console.ReadLine();
+                if(name.Length==0)
+                {
+                    Console.WriteLine("No puedes tener nombre vacío");
+                }
+            }
+            return name;
+        }
+
+        private int GetPositiveNumber(string message)
+        {
+            int n = 0;
+            while (n <= 0)
+            {
+                Console.WriteLine(message);
+                n = int.Parse(Console.ReadLine());
+                if (n <= 0)
+                {
+                    Console.WriteLine("Tiene que ser positivo");
+                }
+            }
+            return n;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DVisualStudio262
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu=new Menu();
+            menu.Execute();
+
+        }
+    }
+}
