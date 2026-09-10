dce82b070a16b72400e918f53391f7aec6f8a056
Ricardo Tanaka <rtanaka@trainbeyond.com>
2026-09-10 17:29:20 -0500

    Added player

---
+++ Player.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller3DVisualStudio262
+{
+    internal class Player
+    {
+    }
+}
